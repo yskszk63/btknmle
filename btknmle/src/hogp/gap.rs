ae5e5a7d03a61af307c149d9fb75af73622e21a6
yskszk63 <yskszk63@gmail.com>
2020-02-22 12:29:10 +0900

    Fix incorrect gap/gatt

--- btknmle/src/hogp/gap.rs
+++ btknmle/src/hogp/gap.rs
@@ -1,10 +1,26 @@
 use super::DatabaseBuilder;
 use crate::gatt::model::Uuid;
-use crate::gatt::{CharacteristicProperties, CCCD};
+use crate::gatt::CharacteristicProperties;
 
 pub(crate) fn add(builder: &mut DatabaseBuilder) {
-    builder.begin_service(Uuid::Uuid16(0x1801));
-    builder.with_characteristic(CharacteristicProperties::INDICATE, Uuid::Uuid16(0x2A05), "");
-    builder.with_user_description("HELLO WORLD!".into());
-    builder.with_cccd(CCCD::empty());
+    builder.begin_service(Uuid::Uuid16(0x1800));
+
+    // Device Name
+    builder.with_characteristic(
+        CharacteristicProperties::READ,
+        Uuid::Uuid16(0x2A00),
+        "btknmle",
+    );
+    // Appearance
+    builder.with_characteristic(
+        CharacteristicProperties::READ,
+        Uuid::Uuid16(0x2A01),
+        vec![0xC2, 0x03],
+    ); // HID mouse
+    // Resolvable Private Address Only
+    builder.with_characteristic(
+        CharacteristicProperties::READ,
+        Uuid::Uuid16(0x2AC9),
+        [0].as_ref(),
+    );
 }
--- btknmle/src/hogp/gatt.rs
+++ btknmle/src/hogp/gatt.rs
@@ -1,22 +1,7 @@
 use super::DatabaseBuilder;
 use crate::gatt::model::Uuid;
-use crate::gatt::CharacteristicProperties;
 
 pub(crate) fn add(builder: &mut DatabaseBuilder) {
-    builder.begin_service(Uuid::Uuid16(0x1800));
-    builder.with_characteristic(
-        CharacteristicProperties::READ,
-        Uuid::Uuid16(0x2A00),
-        "btknmle",
-    );
-    builder.with_characteristic(
-        CharacteristicProperties::READ,
-        Uuid::Uuid16(0x2A01),
-        vec![0xC2, 0x03],
-    ); // HID mouse
-    builder.with_characteristic(
-        CharacteristicProperties::READ,
-        Uuid::Uuid16(0x2A02),
-        [1].as_ref(),
-    );
+    // Generic Attirbute
+    builder.begin_service(Uuid::Uuid16(0x1801));
 }

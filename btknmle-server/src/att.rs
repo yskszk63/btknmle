6d3fa9a5a15be2ad6bfd5719542ecfcb481e5699
yskszk63 <yskszk63@gmail.com>
2020-03-09 23:26:38 +0900

    test att & mgmt

--- btknmle-server/src/att.rs
+++ btknmle-server/src/att.rs
@@ -25,9 +25,43 @@ impl Decoder for AttCodec {
     type Error = std::io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if buf.is_empty() {
+            return Ok(None);
+        }
+
         let result =
             Self::Item::unpack(buf).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         debug!("< {:?}", result);
         Ok(Some(result))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test() {
+        use crate::pkt::att::WriteResponse;
+        use futures::sink::SinkExt;
+        use futures::stream::StreamExt;
+        use tokio::net::UnixStream;
+        use tokio_util::codec::{FramedRead, FramedWrite};
+
+        let (socka, sockb) = UnixStream::pair().unwrap();
+
+        let mut socka = FramedRead::new(socka, AttCodec);
+        let mut sockb = FramedWrite::new(sockb, AttCodec);
+
+        let n = 1024;
+        tokio::spawn(async move {
+            for _ in 0..n {
+                sockb.send(WriteResponse::new().into()).await.unwrap();
+            }
+        });
+
+        for _ in 0..n {
+            socka.next().await.unwrap().unwrap();
+        }
     }
 }

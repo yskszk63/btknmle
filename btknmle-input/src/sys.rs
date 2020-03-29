3b193da490f02dc2c37ab7fb24aeafc348475523
yskszk63 <yskszk63@gmail.com>
2020-03-29 17:37:13 +0900

    prebuilt bindgen

--- btknmle-input/src/sys.rs
+++ btknmle-input/src/sys.rs
@@ -4,10 +4,20 @@
 #![allow(dead_code)]
 #![allow(clippy::all)]
 
+#[cfg(not(bindgen))]
+#[path = "bindgen/linux_input.rs"]
+pub(crate) mod linux_input;
+
+#[cfg(not(bindgen))]
+#[path = "bindgen/linux_input_event_codes.rs"]
+pub(crate) mod linux_input_event_codes;
+
+#[cfg(bindgen)]
 pub(crate) mod linux_input {
     include!(concat!(env!("OUT_DIR"), "/linux_input.rs"));
 }
 
+#[cfg(bindgen)]
 pub(crate) mod linux_input_event_codes {
     include!(concat!(env!("OUT_DIR"), "/linux_input_event_codes.rs"));
 }

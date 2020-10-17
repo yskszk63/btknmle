bbaeae050d9df8fd42112024c491dd081047c0b2
yskszk63 <yskszk63@gmail.com>
2020-10-17 10:04:51 +0900

    fmt

--- btknmle-input/input-sys/src/lib.rs
+++ btknmle-input/input-sys/src/lib.rs
@@ -2,24 +2,27 @@
 
 extern crate libc;
 
-#[cfg(feature="libinput_1_15")]
-#[lazylink::lazylink("input", include="input.rs/input-sys/src/gen_1_15.rs")]
-mod gen {
-}
+#[cfg(feature = "libinput_1_15")]
+#[lazylink::lazylink("input", include = "input.rs/input-sys/src/gen_1_15.rs")]
+mod gen {}
 
-#[cfg(all(feature="libinput_1_14", not(feature="libinput_1_15")))]
-#[lazylink::lazylink("input", include="input.rs/input-sys/src/gen_1_14.rs")]
-mod gen {
-}
+#[cfg(all(feature = "libinput_1_14", not(feature = "libinput_1_15")))]
+#[lazylink::lazylink("input", include = "input.rs/input-sys/src/gen_1_14.rs")]
+mod gen {}
 
-#[cfg(all(feature="libinput_1_11", not(any(feature="libinput_1_14", feature="libinput_1_15"))))]
-#[lazylink::lazylink("input", include="input.rs/input-sys/src/gen_1_11.rs")]
-mod gen {
-}
+#[cfg(all(
+    feature = "libinput_1_11",
+    not(any(feature = "libinput_1_14", feature = "libinput_1_15"))
+))]
+#[lazylink::lazylink("input", include = "input.rs/input-sys/src/gen_1_11.rs")]
+mod gen {}
 
-#[cfg(all(not(any(feature="libinput_1_11", feature="libinput_1_14", feature="libinput_1_15"))))]
-#[lazylink::lazylink("input", include="input.rs/input-sys/src/gen_1_9.rs")]
-mod gen {
-}
+#[cfg(all(not(any(
+    feature = "libinput_1_11",
+    feature = "libinput_1_14",
+    feature = "libinput_1_15"
+))))]
+#[lazylink::lazylink("input", include = "input.rs/input-sys/src/gen_1_9.rs")]
+mod gen {}
 
 pub use gen::*;
--- btknmle-input/libudev-sys/src/lib.rs
+++ btknmle-input/libudev-sys/src/lib.rs
@@ -1,5 +1,4 @@
-#[lazylink::lazylink("udev", include="libudev-sys/src/lib.rs")]
-mod sys {
-}
+#[lazylink::lazylink("udev", include = "libudev-sys/src/lib.rs")]
+mod sys {}
 
 pub use sys::*;
